40a172d951b4b0c588f87bbdb4ceef7b71ebddea
Daniel López <daniel.lopez.puig@gmail.com>
2025-04-13 14:01:38 +0200

    fix: passing stamped as True in launch argument under teleop_keyboard.launch.py as jazzy needs it

--- andino_bringup/launch/teleop_keyboard.launch.py
+++ andino_bringup/launch/teleop_keyboard.launch.py
@@ -38,6 +38,7 @@ def generate_launch_description():
             name='teleop_twist_keyboard_node',
             output='screen',
             prefix = 'xterm -e',
+            parameters=[{'stamped': True}]
          )
 
     return LaunchDescription([
